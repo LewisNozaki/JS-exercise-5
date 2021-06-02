5b01bfaf93f419ed688f0668f30e00b5077bb7ef
Lewis Nozaki <lhnozaki@outlook.com>
2021-06-01 22:13:08 -1000

    updates

--- exercise.js
+++ exercise.js
@@ -1,8 +1,16 @@
 "use strict";
 
-var moveZeros = function (arr) {
+const moveZeros = arr => {
   // TODO: Program me
+  let zeroArr = [];
+
+  let filteredArr = arr.filter(item => {
+    if (item === 0) {
+      zeroArr.push(item);
+    }
+
+    return (item !== 0);
+  });
   
-  
-  return arr;
+  return [...filteredArr, ...zeroArr];
 }
